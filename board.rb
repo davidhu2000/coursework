aab008fbebffd41eff4bb2108805c24b733e9c9c
David Hu <davidhu314@gmail.com>
2016-11-17 14:55:03 -0800

    add method to seed numbers in board

--- board.rb
+++ board.rb
@@ -2,6 +2,8 @@
 
 class Board
   attr_reader :board
+  DELTAS = [[-1, -1], [-1, 0], [-1, 1], [0, 1],
+            [1, 1],   [1, 0],  [1, -1], [0, -1]].freeze
 
   def self.empty_board(height = 10, width = 10)
     Array.new(height) { Array.new(width) }
@@ -17,15 +19,29 @@ class Board
       row = rand(board.length)
       col = rand(board.first.length)
 
-
-
       unless board[row][col] == "\u1F4A3"
         board[row][col] = "\u1F4A3"
         placed += 1
       end
-
     end
-    p board
+    board
+  end
+
+  def self.seed_numbers
+    board = self.seed_bombs
+
+    board.each_with_index do |row, row_idx|
+      board.each_with_index do |el, col_idx|
+
+        if board[row_idx][col_idx].nil?
+          neighbors = find_neighbors(row_idx, col_idx)
+        end
+      end
+    end
+  end
+
+  def self.find_neighbors(row_idx, col_idx)
+
   end
 
   def initialize(board)
