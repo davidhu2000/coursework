32c0a4a45bec34d4cebea45e9e6ab54446a41dfb
David Hu <davidhu314@gmail.com>
2016-11-23 13:55:13 -0800

    Create Deck class and specs

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,31 @@
 require_relative 'card'
 
 class Deck
-  SUITS = [:spade, :heart, :club, :diamond]
+  SUITS = [:spade, :heart, :club, :diamond].freeze
   VALUES = [:a] + (2..13).to_a
 
   attr_reader :cards
 
 
-  def intialize
+  def initialize
     @cards = create_deck
   end
 
   def create_deck
+    new_deck = []
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        new_deck << Card.new(value, suit)
+      end
+    end
 
+    new_deck
   end
+
+  def shuffle!
+    @cards.shuffle!
+  end
+
+
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,15 +2,41 @@ require 'rspec'
 require 'deck'
 
 describe Deck do
+
   subject(:deck) { Deck.new }
 
   describe '#initialize' do
 
-    it 'should create an array of 52 instance of Card' do
+    it 'should create an array of 52 instances of Card' do
+
       expect(deck.cards).to be_an(Array)
       expect(deck.cards.length).to eq(52)
-      expect(deck.cards.first).to be_a(Card)
+      expect(deck.cards[rand(52)]).to be_a(Card)
+
     end
 
+    context "Deck has the right cards" do
+      it "has 13 cards per suit"  do
+        [:spade, :heart, :club, :diamond].each do |suit|
+          suit_count = deck.cards.count { |card| card.suit == suit }
+          expect(suit_count).to eq(13)
+        end
+      end
+
+      it "has 4 cards per value" do
+        [:a, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].each do |value|
+          value_count = deck.cards.count { |card| card.value == value }
+          expect(value_count).to eq(4)
+        end
+      end
+    end
+  end
+
+  describe '#shuffle' do
+    it 'should shuffle the deck' do
+      original = deck.cards.dup
+      deck.shuffle!
+      expect(deck.cards).to_not eq(original)
+    end
   end
 end
