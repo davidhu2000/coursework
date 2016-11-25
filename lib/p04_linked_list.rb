f9e91575202d6304eec7ac9033c9746d68cd1009
David Hu <davidhu314@gmail.com>
2016-11-25 14:22:58 -0800

    Complete phase 4: Linked List

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -13,13 +13,21 @@ class Link
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    self.prev.next = self.next
+    self.next.prev = self.prev
+    @next = nil
+    @prev = nil
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +36,62 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    loop_link(key).val
   end
 
   def include?(key)
+    loop_link(key) != @tail
   end
 
   def append(key, val)
+    link = Link.new(key, val)
+    link.next = @tail
+    link.prev = @tail.prev
+    @tail.prev = link
+    link.prev.next = link
+  end
+
+  def loop_link(key)
+    place = @head.next
+    until place.key == key || place == @tail
+      place = place.next
+    end
+    place
   end
 
   def update(key, val)
+    link = loop_link(key)
+    link.val = val unless link == @tail
   end
 
   def remove(key)
+    link = loop_link(key)
+    raise "No key in list" if link == @tail
+    link.remove
   end
 
   def each
+    place = @head.next
+    until place == @tail
+      yield place
+      place = place.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
