cf164aa113b8def1f00fb2c2cd1d26cbdd76206d
David Hu <davidhu314@gmail.com>
2016-11-30 21:25:49 -0800

    Minor indentation fixes

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -24,34 +24,32 @@ class ShortenedUrl < ActiveRecord::Base
   end
 
   belongs_to :submitter,
-    primary_key: :id,
-    foreign_key: :submitter_id,
-    class_name: :User
+             primary_key: :id,
+             foreign_key: :submitter_id,
+             class_name: :User
 
   has_many :visits,
-    dependent: :destroy,
-    primary_key: :id,
-    foreign_key: :short_url_id,
-    class_name: :Visit
+           dependent: :destroy,
+           primary_key: :id,
+           foreign_key: :short_url_id,
+           class_name: :Visit
 
   has_many :visitors,
-    Proc.new { distinct },
-    dependent: :destroy,
-    through: :visits,
-    source: :visitor
+           proc { distinct },
+           dependent: :destroy,
+           through: :visits,
+           source: :visitor
 
   has_many :tag_ids,
-    dependent: :destroy,
-    primary_key: :id,
-    foreign_key: :url_id,
-    class_name: :Tagging
+           dependent: :destroy,
+           primary_key: :id,
+           foreign_key: :url_id,
+           class_name: :Tagging
 
   def self.random_code
-    code = SecureRandom::urlsafe_base64
+    code = SecureRandom.urlsafe_base64
 
-    while self.exists?(short_url: code)
-      code = SecureRandom::urlsafe_base64
-    end
+    code = SecureRandom.urlsafe_base64 while exists?(short_url: code)
     code
   end
 
@@ -62,15 +60,15 @@ class ShortenedUrl < ActiveRecord::Base
   end
 
   def self.create_for_user_and_long_url!(user, long_url)
-    self.create!(submitter_id: user.id, short_url: random_code, long_url: long_url)
+    create!(submitter_id: user.id, short_url: random_code, long_url: long_url)
   end
 
   def self.submissions_last_minute
-    self.select(:submitter_id).where('created_at > ?', 1.minute.ago)
+    select(:submitter_id).where('created_at > ?', 1.minute.ago)
   end
 
   def self.number_of_submissions_by_user(id)
-    self.select(:submitter_id).where(submitter_id: id).count
+    select(:submitter_id).where(submitter_id: id).count
   end
 
   def num_clicks
@@ -78,14 +76,14 @@ class ShortenedUrl < ActiveRecord::Base
   end
 
   def num_uniques
-    self.visitors.count
+    visitors.count
   end
 
   def num_recent_uniques
-    get_all_visits.where("created_at > ?", 10.minutes.ago)
+    get_all_visits.where('created_at > ?', 10.minutes.ago)
   end
 
   def get_all_visits
-    Visit.select(:visitor_id).where(short_url_id: self.id)
+    Visit.select(:visitor_id).where(short_url_id: id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,20 @@ class User < ActiveRecord::Base
   validates :premium, inclusion: { in: [true, false] }
 
   has_many :submitted_urls,
-    dependent: :destroy,
-    primary_key: :id,
-    foreign_key: :submitter_id,
-    class_name: :ShortenedUrl
+           dependent: :destroy,
+           primary_key: :id,
+           foreign_key: :submitter_id,
+           class_name: :ShortenedUrl
 
   has_many :visits,
-    dependent: :destroy,
-    primary_key: :id,
-    foreign_key: :visitor_id,
-    class_name: :Visit
+           dependent: :destroy,
+           primary_key: :id,
+           foreign_key: :visitor_id,
+           class_name: :Visit
 
   has_many :visited_urls,
-    Proc.new { distinct },
-    dependent: :destroy,
-    through: :visits,
-    source: :visited_url
+           proc { distinct },
+           dependent: :destroy,
+           through: :visits,
+           source: :visited_url
 end
