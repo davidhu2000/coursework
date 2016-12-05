0f86a482442225d8d9ba01dc68035a0bacf4d009
David Hu <davidhu314@gmail.com>
2016-12-05 13:16:08 -0800

    ContactsAPI: Add contacts and contact_share models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  User.create!(username: Faker::Name.name)
+end
+
+5.times do |i|
+  3.times do
+    Contact.create!(name: Faker::Name.name, email: Faker::Internet.email, user_id: i)
+  end
+end
+
+5.times do |i|
+  2.times do
+    ContactShare.create!(user_id: i, contact_id: ((1..5).to_a - [i+1]).sample)
+  end
+end
