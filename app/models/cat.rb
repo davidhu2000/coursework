983bfbc3ada961ae46b1e5c31660c45eb96d072b
David Hu <davidhu314@gmail.com>
2016-12-06 10:12:51 -0800

    ninety_nine_cats initial commit

---
+++ app/models/cat.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Cat < ActiveRecord::Base
+  COLORS = [:orange, :black, :white, :brown, :grey].freeze
+  validates :birth_date, presence: true
+  validates :name, presence: true
+  validates :color, presence: true, inclusion: COLORS
+  validates :sex, presence: true, inclusion: ['M', 'F']
+
+  def age
+    birth_year = self.birth_date.year
+    Time.now.year - birth_year
+  end
+end
