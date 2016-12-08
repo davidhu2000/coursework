db57ab55fc50ffe5f59dc1f1fe58da07aa3f6d85
David Hu <davidhu314@gmail.com>
2016-12-08 10:50:00 -0800

    Add user authenticaiton

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def index
+    redirect_to user_url(current_user) if current_user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: session_params[:email])
+    if @user && @user.is_password?(session_params[:password])
+      login(@user)
+      flash[:success] = ['Welcome to the MusicApp']
+      redirect_to user_url(@user)
+    else
+      flash[:danger] = ['Invalid Credentials, try again']
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_url
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = ['Welcome to the Music App.']
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:danger] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
