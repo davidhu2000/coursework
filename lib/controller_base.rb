c0c68af21e6a8f61d78de2bdd47cc3703040b18b
David Hu <davidhu314@gmail.com>
2016-12-13 14:22:05 -0800

    Complete p03_template_spec

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -39,7 +40,11 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
-    render_content('All The Cats', "text/html")
+    filename = "views/#{self.class.name.underscore}/#{template_name}.html.erb"
+
+    file = File.open(filename, 'r')
+    erb = ERB.new(file.read)
+    render_content(erb.result(binding), "text/html")
   end
 
   # method exposing a `Session` object
