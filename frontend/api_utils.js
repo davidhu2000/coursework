da93b3c1caf0d8eec58d93707bdb5f73d790148a
David Hu <davidhu314@gmail.com>
2016-12-21 15:44:44 -0800

    Add search functionality

--- frontend/api_utils.js
+++ frontend/api_utils.js
@@ -13,7 +13,24 @@ const APIUtil = {
       url: `/users/${id}/follow/`,
       dataType: "json"
     })
-  )
+  ),
+
+  searchUsers: (queryVal, success) => {
+    $.ajax({
+      method: 'get',
+      url: '/users/search',
+      dataType: 'json',
+      data: { query: queryVal },
+      success: res => {
+        console.log('success');
+        success(res);
+      },
+      error: err => {
+        console.log('issues');
+        console.log(err);
+      }
+    });
+  }
 };
 
 module.exports = APIUtil;
---
+++ frontend/user_search.js
@@ -0,0 +1,38 @@
+const API = require('./api_utils.js');
+const FollowToggle = require('./follow_toggle.js');
+
+const UsersSearch = function ($el) {
+    this.$el = $el;
+    this.input = $el.find("input");
+    this.ul = $el.find("ul");
+    this.handleInput();
+};
+
+UsersSearch.prototype.handleInput = function () {
+  this.$el.on('keyup', (e) => {
+    e.preventDefault();
+    let query = this.input.val();
+    console.log('searching');
+    console.log(query);
+    API.searchUsers(query, this.updateList);
+  });
+};
+
+UsersSearch.prototype.updateList = function (users) {
+  $("nav ul").empty();
+  console.log('list');
+  console.log(users);
+  users.forEach( user => {
+    $("nav ul").append(`<li>
+      <a href='/users/${user.id}'>${user.username}</a>
+      <button type="button" class="follow-toggle" name=""
+       data-user-id="${user.id}"
+       data-initial-follow-state="${user.followed}">
+      </button>
+    </li>`);
+
+    new FollowToggle($('#user-search ul li:last-child button'));
+  });
+};
+
+module.exports = UsersSearch;
