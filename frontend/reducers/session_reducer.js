14cd336cd761b5d1715699dfe4b37a2b6d003433
David Hu <davidhu314@gmail.com>
2016-12-29 17:12:16 -0800

    create session_reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,36 @@
+import { merge } from 'lodash';
+
+import { RECEIVE_CURRENT_USER,
+         RECEIVE_ERRORS,
+       } from '../actions/session_actions';
+
+const _defaultState = {
+  session: {
+    currentUser: null,
+    errors: []
+  }
+};
+
+const SessionReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        session: {
+          currentUser: action.currentUser,
+          errors: []
+        }
+      };
+    case RECEIVE_ERRORS:
+      return {
+        session: {
+          currentUser: null,
+          errors: action.erorrs
+        }
+      };
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
