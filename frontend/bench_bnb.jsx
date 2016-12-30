fcb4a9ab43111fe40e9c1eeb667270cc187f01e6
David Hu <davidhu314@gmail.com>
2016-12-29 17:32:20 -0800

    add store.js, fixed rootReducer typo

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, logout, signup } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+
   ReactDOM.render(<h1>BenchBnB</h1>, root);
 
+  // for testing
+  window.store = store;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
