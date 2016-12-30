1ff64ab1db89c5922c9776cfcea0b3ca898b7286
David Hu <davidhu314@gmail.com>
2016-12-29 17:44:21 -0800

    add app and root to components

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
 
-  ReactDOM.render(<h1>BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 
   // for testing
   window.store = store;
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
 });
