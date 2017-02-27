3132a2d9b81262eedd528e29fdab2cd82e2ae21e
David Hu <davidhu314@gmail.com>
2017-02-27 09:25:13 -0800

    remove commented out code

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,19 +3,6 @@ require_relative '01_sql_object'
 require_relative 'relation'
 
 module Searchable
-  # def where(params)
-  #   @relation.where(params)
-  #   # attr_names = params.map { |attr_name, _| "#{attr_name} = ?" }.join(' AND ')
-  #   #
-  #   # res = DBConnection.execute2(<<-SQL, *params.values)
-  #   #   SELECT *
-  #   #   FROM #{self.table_name}
-  #   #   WHERE #{attr_names}
-  #   # SQL
-  #   #
-  #   # self.parse_all(res.drop(1))
-  #   self
-  # end
 
   [:select, :group, :limit, :order, :offset].each do |name|
     define_method(name) do |*params|
